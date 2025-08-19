a7a3a0394ce2daac0165232732cd48f5fedb5d0a
port-orange <jero.a.pardo@gmail.com>
2025-08-20 05:40:49 +0800

    create database handler

--- pyjot/database.py
+++ pyjot/database.py
@@ -1,7 +1,9 @@
 from configparser import ConfigParser
 from pathlib import Path
+from typing import Any, Dict, List, NamedTuple
+import json
 
-from pyjot import DB_EXISTS_ERROR, SUCCESS
+from pyjot import SUCCESS, DB_EXISTS_ERROR, JSON_ERROR, DB_READ_ERROR, DB_WRITE_ERROR
 
 DEFAULT_DB_FILE_PATH = Path.home().joinpath("." + Path.home().stem + "_todo.json")
 
@@ -18,3 +20,31 @@ def init_database(db_path: Path) -> int:
         return SUCCESS
     except OSError:
         return DB_EXISTS_ERROR
+
+
+class DBResponse(NamedTuple):
+    todo_list: List[Dict[str, Any]]
+    error: int
+
+
+class DatabaseHandler:
+    def __init__(self, db_path: Path) -> None:
+        self._db_path = db_path
+
+    def read_todos(self) -> DBResponse:
+        try:
+            with self._db_path.open("r") as db:
+                try:
+                    return DBResponse(json.load(db), SUCCESS)
+                except json.JSONDecodeError:
+                    return DBResponse([], JSON_ERROR)
+        except OSError:
+            return DBResponse([], DB_READ_ERROR)
+
+    def write_todos(self, todo_list: List[Dict[str, Any]]) -> DBResponse:
+        try:
+            with self._db_path.open("w") as db:
+                json.dump(todo_list, db, indent=4)
+            return DBResponse(todo_list, SUCCESS)
+        except OSError:
+            return DBResponse([], DB_WRITE_ERROR)
